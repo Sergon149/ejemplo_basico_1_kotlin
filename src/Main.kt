6fe8aeae911dc6fcc82e3362eebb72e3536e1bb2
carlosbellmont <carlos.bellmont@gmail.com>
2021-07-06 18:05:53 +0200

    Ejemplo focalizado en funciones

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,27 @@
 fun main(args: Array<String>) {
     println("Empezamos!")
     funWithoutParams()
-    funWithParams(5)
+    funWithOneParam(5)
     val myVariableInMain = 10 // myVariableInMain nace
-    funWithParams(myVariableInMain) // myVariableInMain puede usarse
+    funWithOneParam(myVariableInMain) // myVariableInMain puede usarse
+    funWithMultipleParams(15, "quince")
+    println(funReturningString(1))
 }
 
 fun funWithoutParams(){
     println("=== Soy la función funWithoutParams ===")
 }
 
-fun funWithParams(number : Int){ // Se crea una varialbe llamada number cuyo valor será el valor que hayamos mandado (5 y 10)
-    println("=== Soy la función funWithParams ===")
+fun funWithOneParam(number : Int){ // Se crea una varialbe llamada number cuyo valor será el valor que hayamos mandado (5 y 10)
+    println("=== Soy la función funWithOneParam ===")
     println("He recibido el número recibido $number")
-
 }
 
+fun funWithMultipleParams(number : Int, text : String){ // Se crea una varialbe llamada number cuyo valor será el valor que hayamos mandado (5 y 10)
+    println("=== Soy la función funWithMultipleParams ===")
+    println("He recibido el número recibido $number y el texto $text")
+}
+
+fun funReturningString(param1 : Long): String{ // Se crea una varialbe llamada number cuyo valor será el valor que hayamos mandado (5 y 10)
+   return "Esta función devuelve este texto y un $param1"
+}
